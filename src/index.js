b97e00c60dda2a9e070e8b09363267d4c32efba2
DimaTurkey <123176664+DimaTurkey@users.noreply.github.com>
2025-05-17 19:32:49 +0300

    last

---
+++ src/index.js
@@ -0,0 +1,63 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
+
+  const deleteBtn = document.querySelector('.delete-info');
+  const addBtn = document.querySelector('.plus-info');
+  const editBtn = document.querySelector('.redag-info');
+  
+  function setLocalStorage(userData) {
+    localStorage.setItem("userInfo", JSON.stringify(userData));
+  }
+  
+  function getFromLocalStorage() {
+    const data = localStorage.getItem("userInfo");
+    return data ? JSON.parse(data) : null;
+  }
+  
+  function deleteAllFromLocalStorage() {
+    localStorage.removeItem("userInfo");
+  }
+  deleteBtn.addEventListener('click', () => {
+    deleteAllFromLocalStorage();
+  });
+  
+  addBtn.addEventListener('click', () => {
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('phone').value;
+    const phone = document.getElementById('email').value;
+    const email = document.getElementById('comment').value;
+    const user = { name, surname, phone, email };
+    setLocalStorage(user);
+  });
+  
+  editBtn.addEventListener('click', () => {
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('phone').value;
+    const phone = document.getElementById('email').value;
+    const email = document.getElementById('comment').value;
+    const user = { name, surname, phone, email };
+    setLocalStorage(user);
+  });
+  const sendBtn = document.querySelector('.modal__button[data-modal-close]');
+
+sendBtn.addEventListener('click', () => {
+  const name = document.getElementById('name').value;
+  const surname = document.getElementById('phone').value;
+  const phone = document.getElementById('email').value;
+  const email = document.getElementById('comment').value;
+  const user = { name, surname, phone, email };
+  setLocalStorage(user);
+});
